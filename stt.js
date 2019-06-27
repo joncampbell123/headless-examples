59be2b1454484168029d466f40a17f1bc584ebdb
Jonathan Campbell <jonathan@castus.tv>
2019-06-27 16:55:34 -0700

    more

--- stt.js
+++ stt.js
@@ -13,9 +13,16 @@ var driver = new webdriver.Builder()
     .setFirefoxOptions(options)
     .build();
 
-driver.manage().window().setSize(1080,1920);
+driver.manage().window().setSize(720,1280); // you know, like a cell phone held vertically!
 driver.get('https://www.tiktok.com/share/video/6644242110230826246');
-//driver.findElement(By.name('q')).sendKeys('webdriver');
+
+var vid_src = undefined;
+driver.findElement(By.id('__APP_ROOT__')).findElement(By.tagName('video')).getAttribute('src').then(
+    function(src) {
+        console.log('Found URL: '+src);
+        vid_src = src;
+    }
+);
 
 //driver.sleep(1000).then(function() {
 //  driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
